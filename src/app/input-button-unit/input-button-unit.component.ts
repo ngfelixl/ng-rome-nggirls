05381dfdcbde702fab17aebc097fcaa8f1d421da
Felix Lemke <flemke.dev@gmail.com>
2021-07-08 14:21:12 +0200

    feat: add todo list

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,24 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
   templateUrl: './input-button-unit.component.html',
   styleUrls: ['./input-button-unit.component.scss']
 })
-export class InputButtonUnitComponent implements OnInit {
+export class InputButtonUnitComponent {
   title: string = 'Hello World';
 
-  constructor() {
-    console.log('in constructor');
-    this.changeTitle('My First Angular App');
-    console.log(this.title);
-  }
-
-  ngOnInit(): void {
-  }
 
   changeTitle(newTitle: string) {
-    console.log(newTitle);
     this.title = newTitle;
   }
 }
