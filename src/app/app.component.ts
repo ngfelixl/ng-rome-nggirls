0ed930698ed8cd0c5da5e913628f481ed5982e24
Felix Lemke <flemke.dev@gmail.com>
2021-07-08 15:16:18 +0200

    refactor: introduce list-manager component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +6,5 @@ import { TodoItem } from './interfaces/todo-item';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-list';
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
-
-  addItem(title: string) {
-    this.todoList.push({ title });
-  }
+  title = 'My To Do List APP';
 }
