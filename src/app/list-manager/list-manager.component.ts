954419aba51013682332acd7f93338efbfc3a8b6
Felix Lemke <flemke.dev@gmail.com>
2021-07-08 15:55:00 +0200

    feat: add todo list service

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
+import { TodoListService } from '../services/todo-list.service';
 
 @Component({
   selector: 'app-list-manager',
   templateUrl: './list-manager.component.html',
   styleUrls: ['./list-manager.component.scss']
 })
-export class ListManagerComponent {
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
+export class ListManagerComponent implements OnInit {
+  todoList!: TodoItem[];
 
-  addItem(title: string) {
-    this.todoList.push({ title });
+  constructor(private todoListService: TodoListService) {}
+
+  ngOnInit() {
+    this.todoList = this.todoListService.getTodoList();
   }
+
+  addItem(title: string): void {
+    this.todoListService.addItem({ title });
+}
 }
